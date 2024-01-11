784bcf691795fe986065bc62628f16adbf20a720
omar0day <elomar10234@gmail.com>
2024-01-11 18:21:37 +0200

    first commit

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css'
+})
+export class AboutComponent {
+  openResume(){
+    window.open('./assets/cv.pdf','_blank');
+  }
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './header/header.component';
+import {AboutComponent} from './about/about.component';
+import {SkillsComponent} from './skills/skills.component';
+import {PortfolioComponent} from './portfolio/portfolio.component';
+import {FooterComponent} from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,HeaderComponent,AboutComponent,SkillsComponent,PortfolioComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
